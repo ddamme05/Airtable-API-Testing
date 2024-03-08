0780edf68606f051b97d48a440525905c4ba0117
ddamme05 <ddamme05@gmail.com>
2024-03-08 11:10:47 -0500

    Made a small little API.

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const Airtable = require("airtable");
+const app = express();
+const port = process.env.PORT;
 
-const Airtable = require('airtable');
 Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: process.env.AIRTABLE_TOKEN
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.AIRTABLE_TOKEN,
 });
 
 var base = Airtable.base(process.env.AIRTABLE_BASE_ID);
 
+app.use(express.json());
 
+app.get("/api/records", (req, res) => {
+  const records = [];
+  base("Volunteers")
+    .select({
+        fields: ['Name', 'Phone']
+    })
+    .eachPage(
+      function page(pageRecords, fetchNextPage) {
+        pageRecords.forEach((record) => {
+          records.push(record.fields);
+        });
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Error retrieving records");
+        }
+        res.status(200).json(records);
+      }
+    );
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
